0203350460f3fa36464dc3a34d66bcdc14438b68
timurdemenev <311683@niuitmo.ru>
2026-02-06 09:19:21 +0300

    Improve approach to detect L1d cache size and line size

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <map>
 #include <new>
 #include <random>
-#include <unordered_map>
+#include <map>
 #include <vector>
 
 #include <unistd.h>
@@ -75,18 +75,20 @@ namespace hw1 {
             return res + start;
         }
 
-        std::size_t run_cache_size_test(std::size_t *buffer, std::size_t bufsize, std::size_t stride, std::size_t assoc, std::size_t tests_count = 5) {
+        double run_cache_size_test(std::size_t *buffer, std::size_t bufsize, std::size_t stride, std::size_t assoc, std::size_t tests_count = 1) {
             using nanos = std::chrono::nanoseconds;
 
-            std::vector<long long> durations;
+            std::size_t step = stride / sizeof(buffer[0]);
+
+            std::vector<double> durations;
             durations.reserve(tests_count);
 
             for (std::size_t i = 0; i < tests_count; ++i) {
-                std::size_t n = 0;
+                double n = 0;
 
                 const std::size_t repeats = 5;
 
-                shuffle_buffer(buffer, bufsize, stride, assoc);
+                shuffle_buffer(buffer, bufsize, step, assoc);
 
                 for (std::size_t j = 0; j < repeats; ++j) {
                     auto t = now();
@@ -95,9 +97,8 @@ namespace hw1 {
 
                     auto dur = now() - t;
 
-                    long long total_access_time = std::chrono::duration_cast<nanos>(dur).count();
-
-                    long time_per_element_ns = (total_access_time + (tmp & 2)) / bufsize;
+                    double total_access_time = std::chrono::duration_cast<nanos>(dur).count();
+                    double time_per_element_ns = (total_access_time + (tmp & 2)) / bufsize;
 
                     n += time_per_element_ns;
                 }
@@ -108,32 +109,35 @@ namespace hw1 {
             return count_median(durations);
         }
 
-        std::size_t run_cache_line_size_test(std::size_t *buffer, std::size_t bufsize, std::size_t stride, std::size_t tests_count, std::size_t warmup = 5) {
+        std::size_t run_cache_line_size_test(std::size_t *buf, std::size_t bufsize, std::size_t stride, std::size_t tests_count, std::size_t warmup = 5) {
+            using unit_t = std::pair<std::size_t, std::size_t>;
             constexpr std::size_t WORD_SIZE = sizeof(void *);
+
+            unit_t *buffer = reinterpret_cast<unit_t *>(buf);
 
             using nanos = std::chrono::nanoseconds;
 
-            std::vector<std::size_t> durations;
+            std::vector<double> durations;
             durations.reserve(tests_count);
 
             const std::size_t count = bufsize / sizeof(buffer[0]);
             const std::size_t step = stride / WORD_SIZE;
 
             for (std::size_t i = 0; i < tests_count; ++i) {
-                const std::size_t repeats = 5;
-                long long n = 0;
+                const std::size_t repeats = 1;
+                double n = 0;
 
                 for (std::size_t r = 0; r < repeats; ++r) {
                     for (std::size_t j = 0; j < warmup; ++j) {
                         for (std::size_t k = 0; k < count; k += step) {
-                            buffer[k] *= k;
+                            buffer[k].second *= buffer[k].first;
                         }
                     }
 
                     auto t = now();
 
                     for (std::size_t j = 0; j < count; j += step) {
-                        buffer[j] *= j;
+                        buffer[j].second *= buffer[j].first;
                     }
 
                     auto dur = now() - t;
@@ -157,70 +161,87 @@ namespace hw1 {
     cache_size_test_result_t test_cache_size() {
         constexpr std::size_t REPEATS = 10;
         constexpr std::size_t BUFFER_SIZE = 4 * 1024 * 1024;
-        constexpr double CACHE_SIZE_THRESHOLD = 1.4;
 
-        using unit_t = std::size_t;
+        constexpr std::size_t MIN_STRIDE = 1024;
+        constexpr std::size_t MAX_STRIDE = 64 * MIN_STRIDE;
 
-        unit_t *buffer = allocate_aligned_buffer(PAGE_SIZE, BUFFER_SIZE);
+        constexpr double CACHE_ASSOC_THRESHOLD = 1.2;
+        constexpr double CACHE_SIZE_THRESHOLD = 1.8;
 
-        std::unordered_map<std::size_t, std::size_t> cache_size_freq;
-        std::unordered_map<std::size_t, std::size_t> cache_assoc_freq;
+        std::size_t *buffer = allocate_aligned_buffer(PAGE_SIZE, BUFFER_SIZE);
+
+        /* Cache assoc determining */
+
+        std::map<std::size_t, std::size_t> cache_assoc_freq;
 
         for (size_t i = 0; i < REPEATS; ++i) {
-            std::cout << "Round " << i + 1 << "/" << REPEATS << " started\n";
+            std::cout << "Round of determining cache associativity " << i + 1 << "/" << REPEATS << " started\n";
 
-            std::vector<std::size_t> cache_size_candidates;
-
-            std::size_t prev_dur = 0;
-            std::size_t prev_cache_size = 0;
+            double prev_dur = 0;
             std::size_t prev_cache_assoc = 0;
 
-            for (std::size_t stride = 1024; stride < 64 * 1024; stride *= 2) {
+            for (std::size_t stride = MIN_STRIDE; stride < MAX_STRIDE; stride *= 2) {
                 prev_dur = 0;
-                prev_cache_size = 0;
                 prev_cache_assoc = 0;
 
-                for (std::size_t assoc = 2; assoc <= 32; assoc += 2) {
-                    const std::size_t dur = run_cache_size_test(buffer, BUFFER_SIZE / sizeof(unit_t), stride / sizeof(unit_t), assoc);
-                    const std::size_t cache_size = assoc * stride;
+                for (std::size_t assoc = 2; assoc <= 24; assoc += 2) {
+                    const double dur = run_cache_size_test(buffer, BUFFER_SIZE, stride, assoc);
 
-                    if (prev_dur > 0 && prev_dur * CACHE_SIZE_THRESHOLD < dur) {
-                        cache_size_candidates.push_back(prev_cache_size);
+                    if (prev_dur > 0 && prev_dur * CACHE_ASSOC_THRESHOLD < dur) {
                         cache_assoc_freq[prev_cache_assoc]++;
                     }
 
                     prev_dur = dur;
-                    prev_cache_size = cache_size;
                     prev_cache_assoc = assoc;
                 }
             }
 
-            std::for_each(cache_size_candidates.begin(), cache_size_candidates.end(), [&cache_size_freq](std::size_t sz){
-                cache_size_freq[sz]++;
-            });
-
-            std::cout << "Round " << i + 1 << "/" << REPEATS << " finished\n\n";
-        }
-
-        using cache_size_best_result_t = std::pair<std::size_t, std::size_t>;
-
-        cache_size_best_result_t cache_size_best_result;
-
-        for (auto&& [cache_size, freq]: cache_size_freq) {
-            if (freq > cache_size_best_result.second) {
-                cache_size_best_result.first = cache_size;
-                cache_size_best_result.second = freq;
-            }
+            std::cout << "Round of determining cache associativity " << i + 1 << "/" << REPEATS << " finished\n\n";
         }
 
         using cache_assoc_best_result_t = std::pair<std::size_t, std::size_t>;
-
         cache_assoc_best_result_t cache_assoc_best_result;
 
         for (auto&& [cache_assoc, freq]: cache_assoc_freq) {
             if (freq > cache_assoc_best_result.second) {
                 cache_assoc_best_result.first = cache_assoc;
                 cache_assoc_best_result.second = freq;
+            }
+        }
+
+        std::cout << "L1 cache associativity determined\n\n";
+
+        /* Cache size determining */
+
+        std::map<std::size_t, std::size_t> cache_size_freq;
+
+        for (std::size_t i = 0; i < REPEATS; ++i) {
+            std::cout << "Round of determining cache size " << i + 1 << "/" << REPEATS << " started\n";
+
+            double prev_dur = 0;
+
+            for (std::size_t stride = MIN_STRIDE; stride < MAX_STRIDE; stride *= 2) {
+                /* Run test with associativity more than determined to try trigger cache miss */
+                const double dur = run_cache_size_test(buffer, BUFFER_SIZE, stride, cache_assoc_best_result.first + 2);
+                const std::size_t cache_size = stride * cache_assoc_best_result.first;
+
+                if (prev_dur > 0 && prev_dur * CACHE_SIZE_THRESHOLD < dur) {
+                    ++cache_size_freq[cache_size];
+                }
+
+                prev_dur = dur;
+            }
+
+            std::cout << "Round of determining cache size " << i + 1 << "/" << REPEATS << " finished\n\n";
+        }
+
+        using cache_size_best_result_t = std::pair<std::size_t, std::size_t>;
+        cache_size_best_result_t cache_size_best_result;
+
+        for (auto&& [cache_size, freq]: cache_size_freq) {
+            if (freq > cache_size_best_result.second) {
+                cache_size_best_result.first = cache_size;
+                cache_size_best_result.second = freq;
             }
         }
 
@@ -240,20 +261,20 @@ namespace hw1 {
         using cache_line_size_t = std::size_t;
         using time_nanos_t = std::size_t;
 
-        constexpr std::size_t BUFFER_SIZE = 8 * 1024 * 1024;
+        constexpr std::size_t BUFFER_SIZE = 256 * 1024 * 1024;
         constexpr std::size_t WORD_SIZE = sizeof(void *);
 
         constexpr cache_line_size_t MIN_CACHE_LINE_SIZE = 2 * WORD_SIZE;
-        constexpr cache_line_size_t MAX_CACHE_LINE_SIZE = 512;
+        constexpr cache_line_size_t MAX_CACHE_LINE_SIZE = 512 * WORD_SIZE;
 
-        constexpr double CACHE_LINE_THRESHOLD = 1.4;
+        constexpr double CACHE_LINE_THRESHOLD = 1.9;
 
         std::map<cache_line_size_t, time_nanos_t> cache_line_loop_time;
 
         size_t *buffer = allocate_aligned_buffer(PAGE_SIZE, BUFFER_SIZE);
 
         for (cache_line_size_t cache_line = MIN_CACHE_LINE_SIZE; cache_line <= MAX_CACHE_LINE_SIZE; cache_line *= 2) {
-            cache_line_loop_time[cache_line] = run_cache_line_size_test(buffer, BUFFER_SIZE, cache_line, 5);
+            cache_line_loop_time[cache_line] = run_cache_line_size_test(buffer, BUFFER_SIZE, cache_line, 1);
         }
 
         delete[] buffer;
@@ -261,6 +282,17 @@ namespace hw1 {
         cache_line_test_result_t test_result{ 0 };
 
         std::pair<cache_line_size_t, time_nanos_t> prev_result{ 0, 0 };
+
+        for (auto &t: cache_line_loop_time) {
+            double prev_time = prev_result.second;
+            time_nanos_t time = t.second;
+
+            if (prev_time > 0) {
+                std::cerr << "[cache-line-info]: " << t.first << " " << t.second << ", k: " << (prev_time / time) << " :: " << (time / prev_time) << '\n';
+            }
+
+            prev_result = t;
+        }
 
         for (auto &t: cache_line_loop_time) {
             time_nanos_t prev_time = prev_result.second;
